b3e868bf6b6b9b4894b8823c401a3d37d94bfcd6
Abdul Sami <154633228+Yousuf-Siddiq@users.noreply.github.com>
2024-01-17 00:59:37 +0500

    learn variables

---
+++ Addidas.dart
@@ -0,0 +1,11 @@
+void main() {
+  String company = "big brand clothes";
+  print(company);
+  print("how many branches are thered");
+  int shop = 7;
+  print(shop);
+  String verification = "is it good to wear branded clothes";
+  print(verification);
+  bool verified = true;
+  print(verified);
+}
