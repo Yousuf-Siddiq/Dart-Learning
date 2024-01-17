6fbd5b034739be962cab652586b417decb87d420
Abdul Sami <154633228+Yousuf-Siddiq@users.noreply.github.com>
2024-01-17 15:34:58 +0500

    learn list

---
+++ list.dart
@@ -0,0 +1,22 @@
+void main() {
+  String nike = "branded shoes";
+  print(nike);
+  print("sizes");
+  List<int> sizes = [
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+  ];
+  print(sizes);
+  print("quality of the shoes");
+  List<String> quality = [
+    "perfect",
+    "durable",
+    "soft",
+    "comfortable",
+  ];
+  print(quality);
+}
