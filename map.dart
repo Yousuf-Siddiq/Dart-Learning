554b6900510f56bb0550a7bea9486b9ef5c4fc92
Abdul Sami <154633228+Yousuf-Siddiq@users.noreply.github.com>
2024-01-19 21:14:13 +0500

    learn map

---
+++ map.dart
@@ -0,0 +1,25 @@
+void main() {
+  String shop = "dry cleaning industry";
+  print(shop);
+  print("how many branch");
+  Map<String, String> branches = {
+    "defence branch": "mohammad ali society branch",
+    "gulshan branch": "hello",
+  };
+  print(branches);
+  List<Map<String, String>> about = [
+    {
+      "1st branch defence face 2": "street no 4" "karachi",
+      "near hot and spicy resturant": "Karachi"
+    },
+    {
+      "2 branch gulshan block ": "near lucky mall karachi",
+      "3 branch mohammad ali siciety": "near ideal bakery karachi"
+    },
+    {
+      "4 branch sindhi muslim society": "factory located in bhdurabad "
+          "head office "
+    },
+  ];
+  print(about);
+}
